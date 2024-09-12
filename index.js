00ef6c3501f8d694f4149d703ca46bf9cf333208
Jean <inovaset.dev1@outlook.com>
2024-09-12 01:04:06 -0300

    juju

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const TABLE_NAME = 'atm-dadosMentorBeta';
 
   const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
+  console.log('Supabaseqqqqqqqqqqqqqqqq');//
   async function fetchData() {
       console.log('Iniciando a consulta ao Supabase...');
       let { data, error } = await supabase
